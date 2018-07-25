518c96423b2b1b04ed7f00fe082ed2487f68bf0b
Lenyn Alcantara <nightzpy@gmail.com>
2018-07-25 16:58:42 -0500

    Config threads for receive calls

--- src/main/java/com/almundo/callcenter/CallcenterApplication.java
+++ src/main/java/com/almundo/callcenter/CallcenterApplication.java
@@ -1,22 +1,37 @@
 package com.almundo.callcenter;
 
 import com.almundo.callcenter.domain.Employee;
+import com.almundo.callcenter.services.Dispatcher;
 import com.almundo.callcenter.services.EmployeeFactory;
 import com.almundo.callcenter.util.Priority;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.PriorityBlockingQueue;
 
 @SpringBootApplication
 public class CallcenterApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CallcenterApplication.class, args);
+        ExecutorService pool = new Executors.newFixedThreadPool(10);
+        pool.invokeAll(this.generateCalls());
+        pool.shutdown();
     }
 
-    private void performCalls() {
-        PriorityBlockingQueue<Employee> employees = this.generateEmployees();
+    private void generateCalls() {
+        Dispatcher dispatcher = new Dispatcher(this.generateEmployees());
+        List<Callable<Employee>> pendingCalls = new ArrayList<>();
+
+        for (int i = 0; i < 50; i++) {
+            pendingCalls.add();
+        }
+
+        return pendingCalls;
     }
 
     private PriorityBlockingQueue<Employee> generateEmployees() {
