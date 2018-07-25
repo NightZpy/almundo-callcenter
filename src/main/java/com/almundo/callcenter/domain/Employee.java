409e582ee095cfd989156b9339d815f95c21498e
Lenyn Alcantara <nightzpy@gmail.com>
2018-07-24 23:18:36 -0500

    Add all domain models

--- src/main/java/com/almundo/callcenter/domain/Employee.java
+++ src/main/java/com/almundo/callcenter/domain/Employee.java
@@ -5,7 +5,7 @@ import lombok.Data;
 @Data
 public class Employee {
     private String name;
-    private Integer priority;
+    protected Integer priority;
 
     public Employee(String name, Integer priority) {
         this.name = name;
