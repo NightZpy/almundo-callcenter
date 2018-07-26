a1f71ee777405c1f9a1cde553743592c75927789
Lenyn Alcantara <nightzpy@gmail.com>
2018-07-26 01:55:25 -0500

    Add test for concurrent and correct employee take a call

--- src/test/java/com/almundo/callcenter/CallCenterAppTests.java
+++ src/test/java/com/almundo/callcenter/CallCenterAppTests.java
@@ -1,16 +1,58 @@
 package com.almundo.callcenter;
 
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import com.almundo.callcenter.domain.Director;
+import com.almundo.callcenter.domain.Employee;
+import com.almundo.callcenter.domain.Operator;
+import com.almundo.callcenter.domain.Supervisor;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.slf4j.LoggerFactory;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+import java.util.List;
+import java.util.concurrent.*;
+
+@Slf4j
 public class CallCenterAppTests {
 
+    private void turnOffLogger() {
+        Logger root = (Logger)LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
+        root.setLevel(Level.ERROR);
+    }
+
     @Test
-    public void contextLoads() {
+    public void tenConcurrent() throws InterruptedException {
+        this.turnOffLogger();
+        CallCenterApp app = new CallCenterApp();
+        List<Callable<Employee>> pendingCalls = app.generateCalls(10);
+        ExecutorService callPool = Executors.newFixedThreadPool(10);
+        List<Future<Employee>> futureEmployees = callPool.invokeAll(pendingCalls);
+
+        assert futureEmployees.size() == 10;
+    }
+
+    @Test
+    public void nineOperatorsThreeSupervisorsOneDirector() throws ExecutionException, InterruptedException {
+        this.turnOffLogger();
+        CallCenterApp app = new CallCenterApp();
+        app.setOperators(9);
+        app.setSupervisors(3);
+
+        List<Callable<Employee>> pendingCalls = app.generateCalls(13);
+        ExecutorService callPool = Executors.newFixedThreadPool(13);
+        List<Future<Employee>> futureEmployees = callPool.invokeAll(pendingCalls);
+        int operators = 0;
+        int supervisors = 0;
+        int directors = 0;
+
+        for (Future<Employee> futureEmployee: futureEmployees) {
+            if (futureEmployee.get() instanceof Operator) operators++;
+            if (futureEmployee.get() instanceof Supervisor) supervisors++;
+            if (futureEmployee.get() instanceof Director) directors++;
+        }
+
+        assert operators == 9 && supervisors == 3 && directors == 1;
     }
 
 }
