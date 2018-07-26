93d81632b2f68ba7c42b1d80288f67aa3d22a8c9
Lenyn Alcantara <nightzpy@gmail.com>
2018-07-26 00:14:30 -0500

    Finish with log

--- src/main/java/com/almundo/callcenter/services/Dispatcher.java
+++ src/main/java/com/almundo/callcenter/services/Dispatcher.java
@@ -1,11 +1,13 @@
 package com.almundo.callcenter.services;
 
 import com.almundo.callcenter.domain.Employee;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.Random;
 import java.util.concurrent.PriorityBlockingQueue;
 import java.util.concurrent.TimeUnit;
 
+@Slf4j
 public class Dispatcher {
 
     private static final int MAX_CALL_TIME = 10;
@@ -14,19 +16,27 @@ public class Dispatcher {
     private PriorityBlockingQueue<Employee> availableEmployeesQueue;
 
     public Dispatcher(PriorityBlockingQueue<Employee> availableEmployeesQueue) {
+        log.info("START:Dispatcher::availableEmployeesQueue=" + availableEmployeesQueue);
         this.availableEmployeesQueue = availableEmployeesQueue;
     }
 
-    public void dispatchCall() throws InterruptedException {
-        this.calling(this.availableEmployeesQueue.take());
+    public Employee dispatchCall() throws InterruptedException {
+        log.info("START:Dispatcher::dispatchCall");
+        Employee assignedEmployee = this.availableEmployeesQueue.take();
+        log.debug("Dispatcher:dispatchCall::assignedEmployee=" + assignedEmployee);
+        this.calling(assignedEmployee);
+
+        return assignedEmployee;
     }
 
     private void calling(Employee assignedEmployee) throws InterruptedException {
-        Thread.sleep(TimeUnit.SECONDS.toMillis(this.callTime()));
+        long callTime = this.callTime();
+        log.debug("START:Dispatcher::calling:::assignedEmployee=" + assignedEmployee + ":::callTime=" + callTime);
+        Thread.sleep(callTime);
         this.availableEmployeesQueue.add(assignedEmployee);
     }
 
-    private int callTime() {
-        return new Random().nextInt(MAX_CALL_TIME - MIN_CALL_TIME) + MIN_CALL_TIME;
+    private long callTime() {
+        return TimeUnit.SECONDS.toMillis(new Random().nextInt(MAX_CALL_TIME - MIN_CALL_TIME) + MIN_CALL_TIME);
     }
 }
